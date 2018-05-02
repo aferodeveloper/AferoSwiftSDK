acf528408e73c9a840aa38983b0ad971e3b678c2
Justin Middleton <jrmiddle@kiban.io>
2018-05-02 13:50:57 -0700

    IOS-1892 Add "SemanticClass" and "SemanticClassRegistry" objects, for managing and applying value transformers based upon semantic descriptions.

--- AferoSwiftSDK/Core/Models/SemanticClass.swift
+++ AferoSwiftSDK/Core/Models/SemanticClass.swift
@@ -8,6 +8,23 @@
 import Foundation
 import CocoaLumberjack
 
+
+@objcMembers class SemanticClassPropertyKey {
+    static let UnitsPerSecond = "unitsPerSecond"
+    static let UTS35FormatString = "UTS35FormatString"
+    static let ISO8601Options = "ISO8601Options"
+    static let TimeZoneIdentifer = "timeZoneIdentifier"
+    static let TimeZoneAbbreviation = "timeZoneAbbreviation"
+}
+
+@objcMembers class SemanticClassIdentifier: NSString {
+    static let ISO8601Date = "ISO8601Date"
+    static let UTS35Date = "UTS35Date"
+    static let UNIXEpochDate = "UNIXEpochSecondsDate"
+    static let UNIXEpochSecondsDate = "UNIXEpochDate"
+    static let UNIXEpochMillisecondsDate = "UNIXEpochMillisecondsDate"
+}
+
 @objc protocol AferoSemanticClassReferencing: class, NSObjectProtocol {
     func semanticClass(for identifier: String) -> AferoAttributeSemanticClassDescriptor?
 }
@@ -18,12 +35,14 @@ infix operator ← :  AdditionPrecedence
 enum AferoSemanticClassError: LocalizedError, CustomNSError, Equatable {
     
     case unrecognizedSemanticClass(String)
-
+    case duplicateSemanticClass(String)
+    
     // MARK: <LocalizedError>
     
     var errorDescription: String? {
         switch self {
         case .unrecognizedSemanticClass(let name): return "\(type(of: self)).Unrecognized semantic class: '\(name)'"
+        case .duplicateSemanticClass(let name): return "\(type(of: self)).Duplicate semantic class: '\(name)'"
         }
     }
     
@@ -32,25 +51,34 @@ enum AferoSemanticClassError: LocalizedError, CustomNSError, Equatable {
     var errorCode: Int {
         switch self {
         case .unrecognizedSemanticClass: return -1
+        case .duplicateSemanticClass: return -2
         }
     }
     
     var errorUserInfo: [String : Any] {
         switch self {
         case .unrecognizedSemanticClass(let name): return ["name": name]
+        case .duplicateSemanticClass(let name): return ["name": name]
         }
     }
     
     static var errorDomain: String {
         return "\(self)"
     }
-
+    
     // MARK: <Equatable>
     
     static func ==(lhs: AferoSemanticClassError, rhs: AferoSemanticClassError) -> Bool {
         switch (lhs, rhs) {
+            
         case let (.unrecognizedSemanticClass(ln), .unrecognizedSemanticClass(rn)):
             return ln == rn
+            
+        case let (.duplicateSemanticClass(ln), .duplicateSemanticClass(rn)):
+            return ln == rn
+            
+        default:
+            return false
         }
     }
     
@@ -69,9 +97,26 @@ enum AferoSemanticClassError: LocalizedError, CustomNSError, Equatable {
             }
         }
         
-        identifierToClassMap[semanticClass.identifier] = semanticClass.builder()
+        guard identifierToClassMap[semanticClass.identifier] == nil else {
+            throw AferoSemanticClassError.duplicateSemanticClass(semanticClass.identifier)
+        }
+        
+        let mySemanticClass = semanticClass.builder()
             .set(classTable: self)
             .build()
+        
+        identifierToClassMap[semanticClass.identifier] = mySemanticClass
+        
+        // Register the value transformer if necessary
+        
+        if
+            ValueTransformer.init(forName: mySemanticClass.valueTransformerName) == nil,
+            let cls = mySemanticClass.valueTransformerClass as? AferoSemanticValueTransformer.Type,
+            let xformer = cls.init(properties: mySemanticClass.properties) as? ValueTransformer {
+            
+            ValueTransformer.setValueTransformer(xformer, forName: mySemanticClass.valueTransformerName)
+            
+        }
         
         return self
     }
@@ -80,12 +125,12 @@ enum AferoSemanticClassError: LocalizedError, CustomNSError, Equatable {
     static func <<- (lhs: AferoSemanticClassTable, rhs: AferoAttributeSemanticClassDescriptor) throws -> AferoSemanticClassTable {
         return try lhs.register(semanticClass: rhs)
     }
-
+    
     @discardableResult
     static func ← (lhs: AferoSemanticClassTable, rhs: AferoAttributeSemanticClassDescriptor) throws -> AferoSemanticClassTable {
         return try lhs <<- rhs
     }
-
+    
     var semanticClasses: LazyMapCollection<[String : AferoAttributeSemanticClassDescriptor], AferoAttributeSemanticClassDescriptor> {
         let ret = identifierToClassMap.values.lazy
         return ret
@@ -102,24 +147,26 @@ enum AferoSemanticClassError: LocalizedError, CustomNSError, Equatable {
 /// Represents properties for a semantic claass representation, and provides a property
 /// inheritance mechanism.
 
-@objcMembers class AferoAttributeSemanticClassDescriptor: NSObject, AferoJSONCoding {
+@objcMembers class AferoAttributeSemanticClassDescriptor: NSObject {
     
     public let identifier: String
     public let isa: [String]
+    public let semanticDescription: String?
     
-    lazy private var _isa: [AferoAttributeSemanticClassDescriptor] = {
+    lazy fileprivate var _isa: [AferoAttributeSemanticClassDescriptor] = {
         return self.isa.compactMap { self.classTable?.semanticClass(for: $0) }
     }()
     
     let properties: [String: NSObject]
     
-    weak var classTable: AferoSemanticClassReferencing?
+    weak private(set) var classTable: AferoSemanticClassReferencing?
     
-    init(identifier: String, isa: [String]? = nil, properties: [String: NSObject]? = nil, classTable: AferoSemanticClassReferencing? = nil) {
+    init(identifier: String, isa: [String]? = nil, properties: [String: NSObject]? = nil, classTable: AferoSemanticClassReferencing? = nil, semanticDescription: String? = nil) {
         self.identifier = identifier
         self.classTable = classTable
         self.isa = isa ?? []
         self.properties = properties ?? [:]
+        self.semanticDescription = semanticDescription
     }
     
     private let _parentPropertyCache: NSCache<NSString, NSObject> = NSCache()
@@ -146,7 +193,7 @@ enum AferoSemanticClassError: LocalizedError, CustomNSError, Equatable {
             }
             
         }
-
+        
         _parentPropertyCache.setObject(NSNull(), forKey: key as NSString)
         return nil
 
@@ -266,4 +313,235 @@ enum AferoSemanticClassError: LocalizedError, CustomNSError, Equatable {
     }
 }
 
+@objc protocol AferoSemanticValueTransformer: class {
+    init?(properties: [String: Any])
+}
+
+/// A ValueTransformer that transforms between strings and `Date` objects.
+///
+/// AferoFixedFormatDateTransformer is used to transform between Afero attribute values
+/// specifing a timestamp/date of some kind, and a `Date` object.
+/// Format strings are specified using [Unicode Technical Standard #35 v21](http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns)
+
+@objcMembers class UTS35DateValueTransformer: ValueTransformer, AferoSemanticValueTransformer {
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSDate.self
+    }
+    
+    override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+    
+    let dateFormatter: DateFormatter = DateFormatter()
+    
+    init(dateFormat: String) {
+        super.init()
+        dateFormatter.dateFormat = dateFormat
+    }
+    
+    required convenience init?(properties: [String : Any]) {
+        guard let formatString = properties[SemanticClassPropertyKey.UTS35FormatString] as? String else {
+            return nil
+        }
+        self.init(dateFormat: formatString)
+    }
+    
+    /// Transforms a string representation of an Afero attribute value
+    /// to a `Date` as per `dateFormat`, if possible.
+    
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let value = value else { return nil }
+        return dateFormatter.date(from: String(describing: value))
+    }
+    
+    /// Transforms a `Date` to its string representation, if possible.
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let date = value as? Date else { return nil }
+        return dateFormatter.string(from: date)
+    }
+    
+}
+
+/// A ValueTransformer that transforms between ISO86501 strings and `Date` objects.
+
+@available(iOS, introduced: 10.0)
+@objcMembers class ISO8601DateValueTransformer: ValueTransformer, AferoSemanticValueTransformer {
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSDate.self
+    }
+    
+    override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+    
+    let dateFormatter = ISO8601DateFormatter()
+    
+    init(options: ISO8601DateFormatter.Options = [], timeZone: TimeZone? = nil) {
+        dateFormatter.formatOptions = options
+        dateFormatter.timeZone = timeZone
+        super.init()
+    }
+    
+    required convenience init?(properties: [String : Any]) {
+        
+        var options: ISO8601DateFormatter.Options = []
+        var timeZone: TimeZone? = nil
+        
+        if let maybeOptions = properties[SemanticClassPropertyKey.ISO8601Options] as? NSNumber {
+            options = ISO8601DateFormatter.Options(rawValue: maybeOptions.uintValue)
+        } else if let maybeOptions = properties[SemanticClassPropertyKey.ISO8601Options] as? Int {
+            options = ISO8601DateFormatter.Options(rawValue: UInt(maybeOptions))
+        } else if let maybeOptions = properties[SemanticClassPropertyKey.ISO8601Options] as? UInt {
+            options = ISO8601DateFormatter.Options(rawValue: maybeOptions)
+        }
+        
+        if let timeZoneIdentifier = properties[SemanticClassPropertyKey.TimeZoneIdentifer] as? String {
+            timeZone = TimeZone(identifier: timeZoneIdentifier)
+        } else if let timeZoneAbbreviation = properties[SemanticClassPropertyKey.TimeZoneAbbreviation] as? String {
+            timeZone = TimeZone(abbreviation: timeZoneAbbreviation)
+        }
+        
+        self.init(options: options, timeZone: timeZone)
+    }
+    
+    /// Transforms a string representation of an Afero attribute value
+    /// to a `Date` as per `dateFormat`, if possible.
+    
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let value = value as? String else { return nil }
+        return dateFormatter.date(from: value)
+    }
+    
+    /// Transforms a `Date` to its string representation, if possible.
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let date = value as? Date else { return nil }
+        return dateFormatter.string(from: date)
+    }
+    
+}
+
+/// A ValueTransformer that transforms between UNIX epoch time and `Date` objects.
+///
+/// AferoFixedFormatDateTransformer is used to transform between Afero attribute values
+/// specifing a timestamp/date of some kind, and a `Date` object.
+/// Format strings are specified using [Unicode Technical Standard #35 v21](http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns)
+
+@objcMembers class UNIXEpochDateValueTransformer: ValueTransformer, AferoSemanticValueTransformer {
+    
+    let unitsPerSecond: TimeInterval
+    
+    init(unitsPerSecond: TimeInterval) {
+        self.unitsPerSecond = unitsPerSecond
+        super.init()
+    }
+    
+    required convenience init?(properties: [String : Any]) {
+        
+        let unitsPerSecond = ((properties[SemanticClassPropertyKey.UnitsPerSecond] as? NSNumber) ?? NSNumber(value: 1.0))?.doubleValue ?? 0
+        
+        guard unitsPerSecond > 0 else {
+            return nil
+        }
+        
+        self.init(unitsPerSecond: unitsPerSecond)
+    }
+    
+    override convenience init() {
+        self.init(unitsPerSecond: 1.0)
+    }
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSDate.self
+    }
+    
+    override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+    
+    /// Transforms a string representation of an Afero attribute value
+    /// to a `Date` as per `dateFormat`, if possible.
+    
+    override func transformedValue(_ value: Any?) -> Any? {
+        
+        guard let value = value else { return nil }
+        
+        let interval: TimeInterval
+        
+        if let maybeNumber = value as? NSNumber {
+            interval = TimeInterval(maybeNumber.int64Value) / unitsPerSecond
+        } else if let maybeLong = value as? Int64 {
+            interval = TimeInterval(maybeLong) / unitsPerSecond
+        } else if let maybeInt = value as? Int {
+            interval = TimeInterval(maybeInt) / unitsPerSecond
+        } else {
+            assert(false, "Expected NSNumber, Int64, or Int value.")
+            return nil
+        }
+        
+        let ret = NSDate(timeIntervalSince1970: interval)
+        return ret
+        
+    }
+    
+    /// Transforms a `Date` to its string representation, if possible.
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let date = value as? NSDate else { return nil }
+        return NSNumber(value: date.timeIntervalSince1970 * unitsPerSecond)
+    }
+    
+}
+
+extension AferoAttributeSemanticClassDescriptor {
+    
+    var valueTransformerName: NSValueTransformerName {
+        return NSValueTransformerName(rawValue: identifier + "ValueTransformer")
+    }
+
+    var valueTransformer: ValueTransformer? {
+        return ValueTransformer(forName: valueTransformerName)
+    }
+    
+    var valueTransformerClass: AnyClass? {
+        
+        let name = "Afero.\(valueTransformerName.rawValue)"
+        if let ret = NSClassFromString(name) {
+            return ret
+        }
+        
+        let ret: AnyClass? = _isa
+            .compactMap { $0.valueTransformerClass }
+            .lazy
+            .first
+
+        return ret
+    }
+    
+}
+
+extension AferoSemanticClassReferencing {
+    
+    func transformer(forSemanticIdentifier semanticIdentifier: String) -> ValueTransformer? {
+        guard let semanticClass = self.semanticClass(for: semanticIdentifier) else { return nil }
+        return semanticClass.valueTransformer
+    }
+    
+    func semanticValue(forNativeValue nativeValue: Any?, withSemanticClass semanticClass: AferoAttributeSemanticClassDescriptor) -> Any? {
+        return semanticClass.valueTransformer?.transformedValue(nativeValue)
+    }
+    
+    func semanticValue(forNativeValue nativeValue: Any?, withSemanticIdentifier semanticIdentifier: String) -> Any? {
+        return transformer(forSemanticIdentifier: semanticIdentifier)?.transformedValue(nativeValue)
+    }
+    
+    func nativeValue(forSemanticValue semanticValue: Any?, withSemanticClass semanticClass: AferoAttributeSemanticClassDescriptor) -> Any? {
+        return semanticClass.valueTransformer?.reverseTransformedValue(semanticValue)
+    }
+    
+    func nativeValue(forSemanticValue semanticValue: Any?, withSemanticIdentifier semanticIdentifier: String) -> Any? {
+        return transformer(forSemanticIdentifier: semanticIdentifier)?.reverseTransformedValue(semanticValue)
+    }
+    
+}
 
--- Examples/AferoSwiftSDKTests/Core/SemanticClassTests.swift
+++ Examples/AferoSwiftSDKTests/Core/SemanticClassTests.swift
@@ -60,7 +60,7 @@ class AferoSemanticClassTableSpec: QuickSpec {
 
             }
             
-            it("should throw if an a referenced parent attribute doesn't exist.") {
+            it("should throw if a referenced parent attribute doesn't exist.") {
                 expect(t.semanticClasses.count) == 0
                 
                 expect {
@@ -70,6 +70,112 @@ class AferoSemanticClassTableSpec: QuickSpec {
                 expect(t.semanticClasses.count) == 1
             }
             
+            it("should throw if registering a duplicate class.") {
+                expect(t.semanticClasses.count) == 0
+                
+                expect {
+                    try t
+                        ← AferoAttributeSemanticClassDescriptor(identifier: "bar")
+                        ← AferoAttributeSemanticClassDescriptor(identifier: "bar")
+                    }.to(throwError(AferoSemanticClassError.duplicateSemanticClass("bar")))
+                
+                expect(t.semanticClasses.count) == 1
+            }
+
+            
+        }
+        
+        describe("transforming values") {
+            
+            let registry = AferoSemanticClassTable()
+            
+            do {
+                
+                try registry ← AferoAttributeSemanticClassDescriptor(
+                    identifier: SemanticClassIdentifier.ISO8601Date,
+                    semanticDescription: "A date in ISO8601 format."
+                )
+
+                try registry ← AferoAttributeSemanticClassDescriptor(
+                    identifier: SemanticClassIdentifier.UNIXEpochDate,
+                    semanticDescription: "A date represented by seconds since 01 Jan 1970 00:00:00 UTC"
+                )
+
+                try registry ← AferoAttributeSemanticClassDescriptor(
+                    identifier: SemanticClassIdentifier.UNIXEpochSecondsDate,
+                    isa: [SemanticClassIdentifier.UNIXEpochDate],
+                    properties: [
+                        SemanticClassPropertyKey.UnitsPerSecond: NSNumber(value: 1.0)
+                    ],
+                    semanticDescription: "A date represented by seconds since 01 Jan 1970 00:00:00 UTC"
+                )
+                
+                try registry ← AferoAttributeSemanticClassDescriptor(
+                    identifier: SemanticClassIdentifier.UNIXEpochMillisecondsDate,
+                    isa: [SemanticClassIdentifier.UNIXEpochSecondsDate],
+                    properties: [
+                        SemanticClassPropertyKey.UnitsPerSecond: NSNumber(value: 1000.0)
+                    ],
+                    semanticDescription: "A date represented by milliseconds since 01 Jan 1970 00:00:00 UTC"
+                )
+                
+                try registry ← AferoAttributeSemanticClassDescriptor(
+                    identifier: SemanticClassIdentifier.UTS35Date,
+                    properties: [
+                        SemanticClassPropertyKey.UTS35FormatString: "EEE, MMM d, ''yy" as NSString
+                    ],
+                    semanticDescription: "A date represented only by short day of week, short month, day, and two-digit year."
+                )
+                
+            } catch {
+                print(String(reflecting: error))
+            }
+            
+            it("Should have registered all types") {
+                expect(registry.semanticClass(for: SemanticClassIdentifier.UNIXEpochSecondsDate)).toNot(beNil())
+                expect(registry.transformer(forSemanticIdentifier: SemanticClassIdentifier.UNIXEpochSecondsDate)).toNot(beNil())
+                expect(registry.semanticClass(for: SemanticClassIdentifier.UNIXEpochMillisecondsDate)).toNot(beNil())
+                expect(registry.transformer(forSemanticIdentifier: SemanticClassIdentifier.UNIXEpochMillisecondsDate)).toNot(beNil())
+                expect(registry.semanticClass(for: SemanticClassIdentifier.UTS35Date)).toNot(beNil())
+                expect(registry.transformer(forSemanticIdentifier: SemanticClassIdentifier.UTS35Date)).toNot(beNil())
+                expect(registry.semanticClass(for: SemanticClassIdentifier.ISO8601Date)).toNot(beNil())
+                expect(registry.transformer(forSemanticIdentifier: SemanticClassIdentifier.ISO8601Date)).toNot(beNil())
+            }
+            
+            let iSecs: Int = 1525121467
+            let nsnSecs = NSNumber(value: iSecs)
+            let i32Secs = Int32(iSecs)
+            let i64Secs = Int64(iSecs)
+            let dsecs = Double(iSecs)
+            
+            let date = Date(timeIntervalSince1970: dsecs)
+            
+            it("Should transform an integer number of seconds into a date") {
+                expect(registry.semanticValue(forNativeValue: iSecs, withSemanticIdentifier: SemanticClassIdentifier.UNIXEpochSecondsDate) as? Date) == date
+                expect(registry.semanticValue(forNativeValue: nsnSecs, withSemanticIdentifier: SemanticClassIdentifier.UNIXEpochSecondsDate) as? Date) == date
+                expect(registry.semanticValue(forNativeValue: i32Secs, withSemanticIdentifier: SemanticClassIdentifier.UNIXEpochSecondsDate) as? Date) == date
+                expect(registry.semanticValue(forNativeValue: i64Secs, withSemanticIdentifier: SemanticClassIdentifier.UNIXEpochSecondsDate) as? Date) == date
+                expect(registry.semanticValue(forNativeValue: dsecs, withSemanticIdentifier: SemanticClassIdentifier.UNIXEpochSecondsDate) as? Date) == date
+            }
+            
+            it("Should transform a date into an integer number of seconds ") {
+                expect(registry.nativeValue(forSemanticValue: date, withSemanticIdentifier:  SemanticClassIdentifier.UNIXEpochSecondsDate) as? NSNumber) == nsnSecs
+            }
+            
+            let iMillis = i64Secs * 1000
+            let nsnMillis = NSNumber(value: iMillis)
+            let dMillis = nsnMillis.doubleValue
+            
+            it("Should transform an integer number of milliseconds into a date") {
+                expect(registry.semanticValue(forNativeValue: iMillis, withSemanticIdentifier: SemanticClassIdentifier.UNIXEpochMillisecondsDate) as? Date) == date
+                expect(registry.semanticValue(forNativeValue: nsnMillis, withSemanticIdentifier: SemanticClassIdentifier.UNIXEpochMillisecondsDate) as? Date) == date
+                expect(registry.semanticValue(forNativeValue: dMillis, withSemanticIdentifier: SemanticClassIdentifier.UNIXEpochMillisecondsDate) as? Date) == date
+            }
+            
+            it("Should transform a date into an integer number of milliseconds ") {
+                expect(registry.nativeValue(forSemanticValue: date, withSemanticIdentifier:  SemanticClassIdentifier.UNIXEpochMillisecondsDate) as? NSNumber) == nsnMillis
+            }
+
         }
     }
 }
@@ -117,8 +223,3 @@ class AferoSemanticClassSpec: QuickSpec {
     
 }
 
-class AferoTimestampSemanticClassSpec: QuickSpec {
-    
-    override func spec() {
-    }
-}
