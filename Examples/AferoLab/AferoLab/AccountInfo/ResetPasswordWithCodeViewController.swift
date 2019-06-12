8a8c78996060ab57868b5c728f71b8d7f41fec5a
Cora Middleton <cora@afero.io>
2019-06-12 16:36:45 -0700

    IOS-2015 Wire up the password change bits.

--- Examples/AferoLab/AferoLab/AccountInfo/ResetPasswordWithCodeViewController.swift
+++ Examples/AferoLab/AferoLab/AccountInfo/ResetPasswordWithCodeViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 import Afero
-import OnePasswordExtension
+import SVProgressHUD
+import LKAlertController
 
-class ResetPasswordWithCodeViewController: UIViewController {
+//import OnePasswordExtension
+
+class ResetPasswordWithCodeViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var resetCodeLabel: UILabel!
     @IBOutlet weak var resetCodeTextField: UITextField!
@@ -28,8 +31,118 @@ class ResetPasswordWithCodeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        onePasswordButton.isHidden = !OnePasswordExtension.shared().isAppExtensionAvailable()
+        updateUI()
     }
     
+    func updateUI() {
+//        onePasswordButton.isHidden = !OnePasswordExtension.shared().isAppExtensionAvailable()
+        
+        let passwordsMatch = self.passwordsMatch
+        let resetCodeComplete = self.resetCodeComplete
+        
+        print("passwordsMatch: \(passwordsMatch) resetCodeComplete: \(resetCodeComplete)")
+        
+        changePasswordButton.isEnabled = passwordsMatch && resetCodeComplete
+    }
+    
+    var passwordsMatch: Bool {
+        return
+            !(newPasswordTextField?.text?.isEmpty ?? true) &&
+                !(verifyPasswordTextField?.text?.isEmpty ?? true) &&
+                (newPasswordTextField?.text == verifyPasswordTextField?.text)
+    }
+    
+    var resetCodeComplete: Bool {
+        return (resetCodeTextField?.text?.count ?? 0) == 6
+    }
+    
+
+    @IBAction func resetCodeEditingChanged(_ sender: Any) {
+        updateUI()
+    }
+    
+    @IBAction func newPasswordEditingChanged(_ sender: Any) {
+        updateUI()
+    }
+    
+    @IBAction func verifyPasswordEditingChanged(_ sender: Any) {
+        updateUI()
+    }
+    
+    @IBAction func changePasswordTapped(_ sender: Any) {
+        
+        let appId = "com.alphaguardian.safeguard"
+
+        guard
+            let resetCode = resetCodeTextField?.text,
+            let password = newPasswordTextField?.text else {
+                print("Nothing to do!")
+                updateUI()
+                return
+        }
+        
+        SVProgressHUD.show(withStatus: NSLocalizedString("Updating password...", comment: "ResetPassword updating password status"))
+        AFNetworkingAferoAPIClient.default.updatePassword(with: password, shortCode: resetCode, appId: appId)
+            .then {
+                () -> Void in
+                SVProgressHUD.dismiss()
+                Alert(
+                    title: NSLocalizedString(
+                        "Password changed.",
+                        comment: "ResetPassword PasswordChanged alert title"
+                    ),
+                    message: NSLocalizedString(
+                        "Your password has been changed; please sign in with your new password.",
+                        comment:"ResetPassword PasswordChanged alert message"
+                    )
+                    ).addAction(
+                        NSLocalizedString(
+                            "OK",
+                            comment: "ResetPassword password changed alert OK"
+                        ),
+                        style: .cancel,
+                        handler: {
+                            [weak self] _ in
+                            self?.performSegue(withIdentifier: "unwindToSignInFromResetPassword", sender: self)
+                    }).show()
+            }.catch {
+                err in
+                SVProgressHUD.dismiss()
+                Alert(
+                    title: NSLocalizedString(
+                        "Error changing password.",
+                        comment: "ResetPassword PasswordChanged alert title"
+                    ),
+                    message: String(
+                        format: NSLocalizedString(
+                            "Your password has not been changed due to an error: %@.",
+                            comment:"ResetPassword PasswordChanged alert message"
+                        ),
+                        String(describing: err))
+                    ).showOkay()
+        }
+    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        let nextTextField: UITextField?
+        
+        switch(textField) {
+        case resetCodeTextField:
+            nextTextField = newPasswordTextField
+        case newPasswordTextField:
+            nextTextField = verifyPasswordTextField
+        default:
+            nextTextField = nil
+            break
+        }
+        
+        textField.resignFirstResponder()
+        nextTextField?.becomeFirstResponder()
+        return false
+        
+    }
     
 }
